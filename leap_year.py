534a24c8635a02801e3c58a595248e7452d4ad76
Delfina Taira <dtaira@mail.austral.edu.ar>
2025-04-14 12:08:57 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
     print("TO DO")
+
+    año_bisiesto = int(input("Ingrese un año: "))
+
+    if año_bisiesto % 4 == 0 and año_bisiesto % 100 != 0 or año_bisiesto % 400 == 0:
+       print(f"El año {año_bisiesto} es bisiesto")
+    else:
+       print(f"El año {año_bisiesto} no es bisiesto")
